8b019998579927be442af093f12eebb464060f87
Florencia Cabrera <aflorenciacabrera@gmail.com>
2019-04-24 16:21:41 -0300

    actualizo

--- database/migrations/2019_04_24_223158_create_caracteristicas_table.php
+++ database/migrations/2019_04_24_223158_create_caracteristicas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCaracteristicasTabla extends Migration
+class CreateCaracteristicasTable extends Migration
 {
     /**
      * Run the migrations.
